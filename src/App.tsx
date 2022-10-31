9c9ed90e3ad624d8f51577fd106ec4576a4162d5
Luciara-Abreu <luciara.abreu@gmail.com>
2022-10-31 16:42:28 -0300

    teste de mudanca

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
   return (
-  <div>
-  <h1>
-  Agenda Aniversariantes
-  </h1>
-  </div>
-  )}
-  
-  export default App
+    <div>
+      <h1>Agenda Aniversariantes</h1>
+    </div>
+  );
+}
+
+export default App;
