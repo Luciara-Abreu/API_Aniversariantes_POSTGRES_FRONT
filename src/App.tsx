1dbb1ace29cceae4db0c5f2f9dea2c3e6feb89e2
Luciara-Abreu <luciara.abreu@gmail.com>
2022-11-02 13:37:12 -0300

    inicio header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from "../src/App/routes";
 
+
 export const App = () => {
   return (
     <div>
---
+++ src/App/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// https://youtu.be/AXKc4WhgLSA - criar header com typeScript
+
+const Header: React.FC = () => {
+
+  return (
+<div>
+  <h1>Header</h1>
+</div>
+  )
+}
+
+export default Header
