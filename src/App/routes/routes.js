67891614a1c7060f72fc40303f4dee06f62e1d29
Luciara-Abreu <luciara.abreu@gmail.com>
2022-11-24 17:00:46 -0300

    Local errado

--- src/App/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-
-export const PrivateRoutes = () => {
-  const { signed } = useContext(AuthGoogleContext);
-  return signed ? <Outlet /> : <Navigate to="/" />;
-};
-//https://www.youtube.com/watch?v=XjNs_OM6m8A
