bf7fdb9da7d3a73e6959f625aa7962133c414b2e
Luciara-Abreu <luciara.abreu@gmail.com>
2022-11-24 14:02:59 -0300

    App refactorings

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from "../src/App/routes";
+
+export const App = () => {
+  return (
+    <div className="ef-glow">
+      <Routes />
+    </div>
+  );
+}
+
