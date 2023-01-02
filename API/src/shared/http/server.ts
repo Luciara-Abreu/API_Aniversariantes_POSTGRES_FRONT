b61c69c19bb1b0d9874d51495b76641a1983346a
Luciara-Abreu <luciara.abreu@gmail.com>
2023-01-02 16:41:06 -0300

    criação do middleware

--- API/src/shared/http/server.ts
+++ API/src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import routes from './routes'
+import AppError from '@shared/errors/AppError'
 
 const app = express()
 
@@ -9,7 +10,18 @@ app.use(express.json())
 
 app.use(routes)
 
-
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      massage: error.message,
+    })
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(3333, () => {
   console.log(`Server started on port ✨✨✨ 3333 ✨✨✨ `)
