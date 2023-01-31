ed5a134083fc9daa8475835454f8535c047d6a20
Luciara-Abreu <luciara.abreu@gmail.com>
2023-01-31 14:34:19 -0300

    add adm typing in express request, send id in req

---
+++ API/src/@types/express/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace Express {
+  export interface Request {
+    adm: {
+      id: string
+      name: string
+    }
+  }
+}
--- API/src/middlewares/AuthMiddleware/isAuthenticated.ts
+++ API/src/middlewares/AuthMiddleware/isAuthenticated.ts
@@ -2,11 +2,16 @@ import AppError from '@shared/errors/AppError'
 import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
 import authConfig from '@config/auth'
+import { request } from 'http'
 /**
  *Esta funcão vai validar se o tokem que estamos recebendo é um tokem criado pela nossa aplicação.
 Caso não seja, ele deve estourar um erro.
  */
-
+interface ITokenPayload {
+  iat: number
+  exp: number
+  sub: string
+}
 function isAuthenticated(req: Request, res: Response, next: NextFunction): void {
   const authHeader = req.headers.authorization
   if (!authHeader) {
@@ -17,6 +22,14 @@ function isAuthenticated(req: Request, res: Response, next: NextFunction): void 
   // aqui ele valida se o tokem foi criado com essa secret
   try {
     const decodeToken = verify(token, authConfig.jwt.Secret)
+    //console.log('decodeToken Aqui ====> ', decodeToken)
+
+    const { sub } = decodeToken as ITokenPayload
+    //vamos sobreescrever o tipo request para add o adm
+    req.adm = {
+      id: sub,
+      name: sub,
+    }
 
     return next()
   } catch {
