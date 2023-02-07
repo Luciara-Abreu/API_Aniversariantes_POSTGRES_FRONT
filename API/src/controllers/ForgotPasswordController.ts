63cb4ece98e66faab5680b2fbdf8b1884ead8265
Luciara-Abreu <luciara.abreu@gmail.com>
2023-02-07 13:39:17 -0300

    Email Send Route - Forgot Password

---
+++ API/src/controllers/ForgotPasswordController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import SendForgotPasswordEmailService from 'src/services/SendForgotPasswordEmailService/sendForgotPasswordEmailService'
+
+class ForgotPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService()
+    await sendForgotPasswordEmail.execute({
+      email,
+    })
+    return res.status(204).json()
+  }
+}
+export default ForgotPasswordController
