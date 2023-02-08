2393f786126e0a2c1a4cad9d221f2d716a710803
Luciara-Abreu <luciara.abreu@gmail.com>
2023-02-08 13:41:58 -0300

    create password reset view

--- API/src/email/HandlebarTemplate.ts
+++ API/src/email/HandlebarTemplate.ts
@@ -1,9 +1,12 @@
 import Handlebars from "handlebars";
 import { IParseMailTemplate } from "src/interfaces/IEmail";
+import fs from 'fs';
 
 
 class HandlebarTemplate {
   public async parse({file, variables}:IParseMailTemplate): Promise<string>{
+const templateFileContent = await fs.promises.readFile(file, {encoding: 'utf8'});
+
     const parseTemplate = Handlebars.compile(file)
 
     return parseTemplate(variables)
