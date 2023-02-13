f94ff558f5c1b135b918a24163d502ca9bceec43
Luciara-Abreu <luciara.abreu@gmail.com>
2023-02-13 14:58:07 -0300

    deleted

--- API/src/modules/repositories/AdmRepository.ts
+++
@@ -1,9 +0,0 @@
-import AppDataSource from 'src/config/data-source'
-import Adm from 'src/entities/AdmEntity'
-
-
-const admRepository = AppDataSource.getRepository(Adm)
-
-
-
-export default admRepository
--- API/src/modules/repositories/PostRepository.ts
+++
@@ -1,8 +0,0 @@
-import AppDataSource from 'src/config/data-source'
-import Post from 'src/entities/PostEntity'
-
-
-const postRepository = AppDataSource.getRepository(Post)
-
-
-export default postRepository
