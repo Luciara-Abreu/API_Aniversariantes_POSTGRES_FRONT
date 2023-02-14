e8c08cd0d58e63ece7fb5c65e4fccd2ae19c395e
Luciara-Abreu <luciara.abreu@gmail.com>
2023-02-14 19:03:30 -0300

    Pagination list birthdays

--- API/src/services/userServices/listAllUserService.ts
+++ API/src/services/userServices/listAllUserService.ts
@@ -1,10 +1,11 @@
 import userRepository from 'src/repositories/UserRepository'
-import  User  from 'src/entities/UserEntity'
+import User from 'src/entities/UserEntity'
 
 class ListAllUserService {
   public async execute(): Promise<User[]> {
     const userRepo = userRepository
-    const users = await userRepo.find()
+    //const users = await userRepo.find()
+    const users = await userRepo.createQueryBuilder('user').take(10).getMany()
     return users
   }
 }
