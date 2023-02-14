c31d26fe349bb3f86f43fc4b5c5abecdc1d5b50e
Luciara-Abreu <luciara.abreu@gmail.com>
2023-02-14 12:02:36 -0300

    Updates in AdmRepository

--- API/src/repositories/AdmRepository.ts
+++ API/src/repositories/AdmRepository.ts
@@ -1,47 +1,29 @@
-import AppError from '@shared/errors/AppError'
 import AppDataSource from 'src/config/data-source'
 import Adm from 'src/entities/AdmEntity'
 
 const AdmRepository = AppDataSource.getRepository(Adm).extend({
-  async findByName(name: string): Promise<Adm | undefined> {
-    /**
- * Desta forma ele trás todo objeto e complica para fazer validações.
-  const adm = await this.findOne({
-      where: {
-        name,
-      },
-    })
-  */
+  async findByName(name: string): Promise<Adm | null> {
     const adm = await this.findOneBy({ name })
-    if (!adm) {
-      throw new AppError('Adm does not exist')
-    }
     return adm
   },
 
-  async findByEmail(email: string): Promise<Adm | undefined> {
+  async findByEmail(email: string): Promise<Adm | null> {
     const adm = await this.findOneBy({ email })
-    if (!adm) {
-      throw new AppError('Adm does not exist')
-    }
     return adm
   },
 
-  async findBybirthDate(birthDate: Date): Promise<Adm | undefined> {
+  async findBybirthDate(birthDate: Date): Promise<Adm | null> {
     const adm = await this.findOneBy({ birthDate })
-    if (!adm) {
-      throw new AppError('Adm does not exist')
-    }
     return adm
   },
 
-  async findById(id: string): Promise<Adm> {
+  async findById(id: string): Promise<Adm | null> {
     const adm = await this.findOneBy({ id })
-
-    if (!adm) {
-      throw new AppError('Adm does not exist')
-    }
-
+    return adm
+  },
+  
+  async findByPassword(password: string): Promise<Adm | null> {
+    const adm = await this.findOneBy({ password })
     return adm
   },
 })
