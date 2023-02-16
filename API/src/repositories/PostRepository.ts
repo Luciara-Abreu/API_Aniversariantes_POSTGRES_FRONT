14579a9589d9d697a6be3a522d105b7a1531a7b0
Luciara-Abreu <luciara.abreu@gmail.com>
2023-02-16 15:59:33 -0300

    filter to make it easier to query a post by getting the userID

--- API/src/repositories/PostRepository.ts
+++ API/src/repositories/PostRepository.ts
@@ -1,8 +1,25 @@
+import AppError from '@shared/errors/AppError'
 import AppDataSource from 'src/config/data-source'
 import Post from 'src/entities/PostEntity'
 
-
-const postRepository = AppDataSource.getRepository(Post)
-
+const postRepository = AppDataSource.getRepository(Post).extend({
+  async findByUserID(userID: string): Promise<Post> {
+    const list = await this.findOneBy({
+      userID,
+    })
+    if (!list) {
+      throw new AppError('Repository-> Dont exist post for user')
+    } else {
+      console.log('Repository userID-> ', list)
+    }
+    return list
+  },
+})
 
 export default postRepository
+
+/*const post = await this.findOne({
+      relations: {
+        user: true,
+      },
+    })*/
